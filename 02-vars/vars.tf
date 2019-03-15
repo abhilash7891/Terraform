4853fe50f949de2436e350e8c8723074f56d6147
unknown <abhilash.madala@gmail.com>
2019-03-15 20:11:17 +0530

    --- Auto-Git Commit ---

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -2,3 +2,9 @@ variable "vpc_cidr_Block"
 {
     default=["10.0.0.0/24"]
 }
+
+variable "subnet_cidr_Block"
+{
+    type ="List"
+    Default="[10.0.0.0/16,10.0.1.0/16,10.0.2.0/16]"
+}
--- 02-vars/vpc.tf
+++ 02-vars/vpc.tf
@@ -2,3 +2,21 @@ resource "aws_vpc" "main"
 {
     cidr_block="${var.vpc_cidr_Block}"
 }
+
+resource "aws_subnet" "subnet1"
+{
+    vpc="${aws_vpc.main.id}"
+    cidr_block="${var.subnet_cidr_Block[0]}"
+}
+
+resource "aws_subnet" "subnet2"
+{
+    vpc="${aws_vpc.main.id}"
+    cidr_block="${var.subnet_cidr_Block[1]}"
+}
+
+resource "aws_subnet" "subnet3"
+{
+    vpc="${aws_vpc.main.id}"
+    cidr_block="${var.subnet_cidr_Block[2]}"
+}
