39315b4ecc3f148f38a8c9a7c16d86256fce18a3
unknown <abhilash.madala@gmail.com>
2019-03-15 21:29:54 +0530

    --- Auto-Git Commit ---

--- 02-vars/provider.tf
+++ 02-vars/provider.tf
@@ -1,6 +1,6 @@
 provider "aws"
 {
     region="us-west-2"
-    shared_credentals_file="/home/centos/.aws/credentials"
+    shared_credentals_file="/home/centos/.aws/credentails"
     profile="Default"
 }
