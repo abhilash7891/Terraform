6bcf77df7eeae9080a8149f53565e635b5230d9d
unknown <abhilash.madala@gmail.com>
2019-03-16 13:06:28 +0530

    --- Auto-Git Commit ---

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -6,5 +6,5 @@ variable "vpc_cidr_Block"
 variable "subnet_cidr_Block"
 {
     type ="List"
-    Default="[10.0.0.0/16,10.0.1.0/16,10.0.2.0/16]"
+    default="[10.0.0.0/16,10.0.1.0/16,10.0.2.0/16]"
 }
