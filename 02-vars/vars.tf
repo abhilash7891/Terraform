b169079b742eae7bd6d565dacc8440c7a1f50d3d
unknown <abhilash.madala@gmail.com>
2019-03-17 09:18:20 +0530

    --- Auto-Git Commit ---

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -9,6 +9,7 @@ variable "subnet_cidr_Block"
     default=["10.0.0.0/16","10.0.1.0/16","10.0.2.0/16"]
 }
 
+data "aws_availability_zones" "main" {}
 variable "tags" {
     type = "map"
 default=
--- 02-vars/vpc.tf
+++ 02-vars/vpc.tf
@@ -8,6 +8,7 @@ resource "aws_subnet" "subnet"
  count=3
     vpc_id="${aws_vpc.main.id}"
     cidr_block="${var.subnet_cidr_Block[count.index]}"
+    azs="${data.aws_availability_zones.main.names[count.index]}"
 tags={
     name="${var.tags["name"]}"
     Project-name="${var.tags["project"]}"
