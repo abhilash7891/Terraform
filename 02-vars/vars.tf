1c502db078d43de04dad471961ba4a43611cd5bf
unknown <abhilash.madala@gmail.com>
2019-03-17 09:01:50 +0530

    --- Auto-Git Commit ---

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -8,3 +8,12 @@ variable "subnet_cidr_Block"
     type ="list"
     default=["10.0.0.0/16","10.0.1.0/16","10.0.2.0/16"]
 }
+
+variable "tags" {
+    type = "map"
+
+    name= "Terraform"
+    project="Terraform-Project"
+    ennvironment="Dev"
+  
+}
---
+++ 02-vars/vpc.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "main"
+{
+    cidr_block="${var.vpc_cidr_Block}"
+}
+
+resource "aws_subnet" "subnet"
+{
+ count=3
+    vpc_id="${aws_vpc.main.id}"
+    cidr_block="${var.subnet_cidr_Block[count.index]}"
+tags={
+    name="${var.tags["name"]}"
+    Project-name="${var.tags["project"]}"
+    project-ENV="${var.tags["environment"]}"
+}
+}
