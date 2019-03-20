e30e089703cdaff12d0ce7013446d79ed8acc263
unknown <abhilash.madala@gmail.com>
2019-03-20 11:52:42 +0530

    --- Auto-Git Commit ---

--- Modules/Network/subnet.tf
+++ Modules/Network/subnet.tf
@@ -10,3 +10,16 @@ tags={
     project-ENV="${ENV}"
 }
 }
+
+resource "aws_subnet" "public_subnet"
+{
+ count="${length(var.public_subnet_cidr_block)}"
+    vpc_id="${aws_vpc.main.id}"
+    cidr_block="${element(var.public_subnet_cidr_block.*.id,count.index)}"
+    availability_zone="${data.aws_availability_zones.main.names[count.index]}"
+tags={
+    name="${var.tags["name"]}"
+    Project-name="${var.tags["project"]}"
+    project-ENV="${ENV}"
+}
+}
