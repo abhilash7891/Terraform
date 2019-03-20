aec0d26c20fd314b2e4ca6290c30ac9ff4b8b4f5
unknown <abhilash.madala@gmail.com>
2019-03-20 11:54:33 +0530

    --- Auto-Git Commit ---

--- Modules/Network/subnet.tf
+++ Modules/Network/subnet.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public_subnet"
 {
  count="${length(var.public_subnet_cidr_block)}"
     vpc_id="${aws_vpc.main.id}"
-    cidr_block="${element(var.public_subnet_cidr_block.*.id,count.index)}"
+    cidr_block="${element(var.public_subnet_cidr_block,count.index)}"
     availability_zone="${data.aws_availability_zones.main.names[count.index]}"
 tags={
     name="${var.tags["name"]}"
