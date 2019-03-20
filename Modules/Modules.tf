ab95c64686a26d42f7a846cc7762c16de3b5d0d5
unknown <abhilash.madala@gmail.com>
2019-03-20 11:55:28 +0530

    --- Auto-Git Commit ---

--- Modules/Modules.tf
+++ Modules/Modules.tf
@@ -2,6 +2,7 @@ Module "Network"
 {
     source="./Modules/Network"
     ENV="${var.ENV}"
+    vpc_cidr_block="${var.vpc_cidr_block}"
     private_subnet_cidr_block ="${var.private_subnet_cidr_block}"
     public_subnet_cidr_block ="${var.public_subnet_cidr_block} "
 
