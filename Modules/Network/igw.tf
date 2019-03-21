7e8b630aaa4578d33a350fdf20b47f5da9be7af5
unknown <abhilash.madala@gmail.com>
2019-03-21 10:07:13 +0530

    --- Auto-Git Commit ---

---
+++ Modules/Network/igw.tf
@@ -0,0 +1,18 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+data "aws_route_table" "Public_route_table" {
+  subnet_id = "${aws_subnet.public_subnet.*.id}"
+}
+
+data "aws_route" "route" {
+  route_table_id         = "${aws_route_table.Public_route_table.*.id}"
+  destination_cidr_block = "10.0.1.0/24"
+}
+
