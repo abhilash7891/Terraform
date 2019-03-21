6e445e9aafab2350a04e7fe97eb843aa0f1a23d1
unknown <abhilash.madala@gmail.com>
2019-03-21 10:11:48 +0530

    --- Auto-Git Commit ---

--- Modules/Network/igw.tf
+++ Modules/Network/igw.tf
@@ -25,3 +25,13 @@ data "aws_route" "route" {
   route_table_id         = "${aws_route_table.Private_route_table.*.id}"
   destination_cidr_block = "10.0.1.0/24"
 }
+
+resource "aws_route_table_association" "Public_RT_Associate" {
+  subnet_id      = "${aws_subnet.public_subnet.id}"
+  route_table_id = "${aws_route_table.Public_route_table.id}"
+}
+
+resource "aws_route_table_association" "Private_RT_Associate" {
+  subnet_id      = "${aws_subnet.private_subnet.id}"
+  route_table_id = "${aws_route_table.Private_route_table.id}"
+}
