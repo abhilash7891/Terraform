4cba1f8807f956b6994335c21602c23261f434bc
unknown <abhilash.madala@gmail.com>
2019-03-22 14:22:21 +0530

    --- Auto-Git Commit ---

--- Modules/Modules.tf
+++ Modules/Modules.tf
@@ -5,8 +5,6 @@ Module "Network"
     vpc_cidr_block="${var.vpc_cidr_block}"
     private_subnet_cidr_block ="${var.private_subnet_cidr_block}"
     public_subnet_cidr_block ="${var.public_subnet_cidr_block} "
-
-
 }
 
 
--- Modules/Network/igw.tf
+++ Modules/Network/igw.tf
@@ -7,24 +7,26 @@ resource "aws_internet_gateway" "gw" {
 }
 
 
-data "aws_route_table" "Public_route_table" {
-  subnet_id = "${aws_subnet.public_subnet.*.id}"
-}
+resource "aws_route_table" "public_RT" {
+  vpc_id = "${aws_vpc.main.id}"
 
-data "aws_route" "route" {
-  route_table_id         = "${aws_route_table.Public_route_table.*.id}"
-  destination_cidr_block = "10.0.1.0/24"
-}
-
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
 
 data "aws_route_table" "Private_route_table" {
   subnet_id = "${aws_subnet.private_subnet.*.id}"
 }
 
-data "aws_route" "route" {
-  route_table_id         = "${aws_route_table.Private_route_table.*.id}"
-  destination_cidr_block = "10.0.1.0/24"
-}
+resource "aws_route_table" "private_RT" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "10.0.2.0/24"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
 
 resource "aws_route_table_association" "Public_RT_Associate" {
   subnet_id      = "${aws_subnet.public_subnet.id}"
