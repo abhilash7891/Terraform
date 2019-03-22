9480094daf0071825bdebd188bfec46ed0bd0542
unknown <abhilash.madala@gmail.com>
2019-03-22 15:00:52 +0530

    --- Auto-Git Commit ---

--- stack/variables.tf
+++ stack/variables.tf
@@ -1,6 +1,12 @@
 variable "vpc_cidr_block" { }
-variable ENV {}
+variable "ENV" { }
 
+variable "Project-Name" { }
+
+variable created-by
+{
+    default="Terraform"
+}
 variable private_subnet_cidr_block {}
 variable public_subnet_cidr_block  {}
 
