3832aec344e108866bb2740724f9e4353dd09ca9
unknown <abhilash.madala@gmail.com>
2019-03-22 17:17:30 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/subnet.tf
+++ stack/Modules/Network/subnet.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_subnet"
  count="${length(var.public_subnet_cidr_block)}"
     vpc_id="${aws_vpc.main.id}"
     cidr_block="${var.public_subnet_cidr_block[count.index]}"
-    --cidr_block="${element(var.public_subnet_cidr_block,count.index)}"
+    /*--cidr_block="${element(var.public_subnet_cidr_block,count.index)}"*/
     availability_zone="${data.aws_availability_zones.azs.names[count.index]}"
     map_public_ip_on_launch=true
 tags={
