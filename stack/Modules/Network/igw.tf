daf3056274ba90184e87528cc6a87fa3997e2ab8
unknown <abhilash.madala@gmail.com>
2019-03-22 16:01:24 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/igw.tf
+++ stack/Modules/Network/igw.tf
@@ -8,7 +8,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = "${aws_vpc.main.id}"
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.gw.id}"
   }
 }
@@ -22,12 +22,14 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-resource "aws_route_table_association" "Public_RT_Associate" {
-  subnet_id      = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route_table.Public_route_table.id}"
+resource "aws_route_table_association" "public-rt-assoc" {
+    count           = "${length(var.public_subnet_cidr_block)}"
+    subnet_id       = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+    route_table_id  = "${aws_route_table.Public_route_table.id}"
 }
 
-resource "aws_route_table_association" "Private_RT_Associate" {
-  subnet_id      = "${aws_subnet.private_subnet.id}"
-  route_table_id = "${aws_route_table.Private_route_table.id}"
+resource "aws_route_table_association" "private-rt-assoc" {
+    count           = "${length(var.private_subnet_cidr_block)}"
+    subnet_id       = "${element(aws_subnet.private-subnet.*.id, count.index)}"
+    route_table_id  = "${aws_route_table.Private_route_table.id}"
 }
