ae6025566781e012bc87c0a785626623c220601d
unknown <abhilash.madala@gmail.com>
2019-03-22 17:01:55 +0530

    --- Auto-Git Commit ---

--- stack/Modules/Network/igw.tf
+++ stack/Modules/Network/igw.tf
@@ -21,13 +21,13 @@ resource "aws_route_table" "private_route_table" {
     gateway_id = "${aws_internet_gateway.gw.id}"
   }
 }
-
+/*
 resource "aws_route_table_association" "public-rt-assoc" {
     count           = "${length(var.public_subnet_cidr_block)}"
     subnet_id       = "${element(aws_subnet.public_subnet.*.id, count.index)}"
     route_table_id  = "${aws_route_table.public_route_table.id}"
 }
-
+*/
 resource "aws_route_table_association" "private-rt-assoc" {
     count           = "${length(var.private_subnet_cidr_block)}"
     subnet_id       = "${element(aws_subnet.private_subnet.*.id, count.index)}"
